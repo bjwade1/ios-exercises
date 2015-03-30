8c8555b5f8570de3004a1a20dc319eb118a35587
Brandon Wade <brandon.wade3@gmail.com>
2015-03-30 17:41:23 -0500

    Completed loops

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -12,7 +12,26 @@
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @"";
+    NSString *returnedString = @"";
+    NSInteger looper = number;
+    
+    while(looper<=otherNumber){
+        returnedString = [NSString stringWithFormat:@"%@%ld", returnedString, looper];
+        looper++;
+    }
+    
+    looper = otherNumber;
+    while (number >= looper) {
+        returnedString = [NSString stringWithFormat:@"%@%ld", returnedString, looper];
+        looper++;
+    }
+
+    looper = number;
+    while(looper==otherNumber){
+        returnedString = [NSString stringWithFormat:@"%ld", looper];
+        looper++;
+    }
+    return returnedString;
 }
 
 @end
