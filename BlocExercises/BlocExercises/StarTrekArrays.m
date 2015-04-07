f6091125e7f107e2dd1856df7663a85f850edc23
Brandon Wade <brandon.wade3@gmail.com>
2015-04-07 00:11:09 -0500

    Partially completed arrays

--- BlocExercises/BlocExercises/StarTrekArrays.m
+++ BlocExercises/BlocExercises/StarTrekArrays.m
@@ -12,22 +12,47 @@
 
 - (NSArray *) arrayOfStarTrekCharactersFromString:(NSString *)characterString {
     /* WORK HERE */
-    return @[];
+    
+    NSArray * trekArray = [characterString componentsSeparatedByString:@";"];
+    
+    return trekArray;
 }
 
 - (NSString *) stringOfStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @"";
+    
+    NSString *worf = [characterArray objectAtIndex:0];
+    NSString *picard = [characterArray objectAtIndex:1];
+    NSString *crusher = [characterArray objectAtIndex:2];
+    
+    NSString *trekString = [NSString stringWithFormat: @"%@;%@;%@", worf, picard, crusher];
+    /*stringFromArray wouldn't work without sorting, because characterArray is in
+     a different order (not alphabetical) than expectedString*/
+    return trekString;
 }
 
 - (NSArray *) alphabeticallySortedStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @[];
+   
+    NSArray *trekArray = [characterArray sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
+    
+    return trekArray;
 }
 
 - (BOOL) characterArrayContainsWorf:(NSArray *)characterArray {
     /* WORK HERE */
-    return NO;
+    
+    NSPredicate *hiWorf = [NSPredicate predicateWithFormat:@"SELF CONTAINS[c] 'Worf'"];
+    
+    [characterArray filteredArrayUsingPredicate:hiWorf];
+    
+    if ([characterArray containsObject:@"Worf"]) {
+        return YES;
+    }
+    
+    else {
+        return NO;
+    }
 }
 
 @end
